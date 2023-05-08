cc4f4e8c52e0e0651a413eafa0edb7c1d20a37e1
Danielle Rivas <85266216+djrivas7400@users.noreply.github.com>
2023-05-08 14:34:43 -0400

    Create insert.ml

---
+++ InClass/insert.ml
@@ -0,0 +1,6 @@
+#Write a recursive function that inserts an element to a sorted list
+
+let rec insert n l = if l=[] then [n] else if n<= List.hd l then n::l else List.hd l::(insert (n) (List.tl l));;
+
+#Example Input:
+  insert 3 [1;2;4;56];;
