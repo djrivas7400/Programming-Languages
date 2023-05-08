81151ec01c6f88bcfeb2c089338640a2fbf4ceaf
Danielle Rivas <85266216+djrivas7400@users.noreply.github.com>
2023-05-08 14:15:07 -0400

    Update length.ml

--- InClass/length.ml
+++ InClass/length.ml
@@ -1,1 +1,8 @@
+#Option 1
 let rec length l = if l=[] then 0 else length(List.tl l)+1;;
+
+#Option 2
+let rec length l = if l=[] then 0 else length(let h::t in t)+1;;
+
+#Example Input:
+#length[5;4;3;2];;
