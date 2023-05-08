ffa6cfe33a38fa5aa26f8c53909f875c82872cc3
Danielle Rivas <85266216+djrivas7400@users.noreply.github.com>
2023-05-08 14:46:49 -0400

    Update addfirsttwolengths.ml

--- InClass/addfirsttwolengths.ml
+++ InClass/addfirsttwolengths.ml
@@ -1,3 +1,7 @@
+#Write a recursive function that returns the sum of the first two lengths of a list of lists
 let rec length l = if l=[] then 0 else length(List.tl l)+1;;
 
 let addfirsttwolengths l = length(List.hd l) + length(List.hd(List.tl l));;
+
+#Example Input:
+  addfirsttwolengths [[1;2];[2];[2;3;4]];;
